6621f7ee5b596ddc646e23f504b5a54292ca8e62
likvidator <max-kor4@yandex.ru>
2013-05-03 01:07:15 +0400

    Test Hash Function

--- Hash.py
+++ Hash.py
@@ -1,7 +1,15 @@
 class Hash:
-    def __init__(self,value):
-        self.key=None
+    def __init__(self,value,key):
+        self.key=key
         self.value=value
 
-
-str(a)[len(str(a))-1:]# mod
+def Hashing(self,value):
+    self=Hash(value)
+    
+#str(a)[len(str(a))-1:] mod
+def Hash_function(value):
+    value=float(value)/3
+    key=int(str(value)[len(str(value))-1:])
+    a=('n'+str(key))
+    Hashing(a,value)
+    
