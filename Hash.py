0b5345c69ac1075a07f96cd002e7b2509cac11dc
likvidator <max-kor4@yandex.ru>
2013-05-08 11:29:38 +0400

    Add Funktion

--- Hash.py
+++ Hash.py
@@ -2,17 +2,24 @@ class init:
     def __init__(self,value,key):
         self.key=key
         self.value=value
+
+def Hashing(value):
+    summ=0
+    for n in str(value):
+        summ+=int(ord(n))
+    return (summ%10)
+
 class Hash:
     def __init__(self):
         self.array=[]
         for i in range(10):
             self.array+=[init(1,2)]
     def add(self,value):
-        key=(value % 10)
+        key=Hashing(value)
         self.array[key].key=key
         self.array[key].value=value
     def delite(self,value):
-        key=(value % 10)
+        key=Hashing(value)
         if self.array[key].value!=None:    
             self.array[key].key=None
             self.array[key].value=None
@@ -20,10 +27,11 @@ class Hash:
         else:
             print "No Delite"
     def search(self,value):
-        key=(value % 10)
+        key=Hashing(value)
         if self.array[key].value!=None:    
             print ("Yes Key=",self.array[key].key)
         else:
             print "No"
-        
+
+
         
