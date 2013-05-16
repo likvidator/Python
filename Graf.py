ba8a4ad39f87a10844aaf417fb543fe9c6d467e2
likvidator <max-kor4@yandex.ru>
2013-05-16 14:06:48 +0400

    Dont't collizi

---
+++ Graf.py
@@ -0,0 +1,6 @@
+class graf:
+    def __init__(self,name):
+        self.name=name
+        self.branch=[]
+
+    
--- Hash.py
+++ Hash.py
@@ -4,50 +4,51 @@ def Hashing(value):
         summ+=int(ord(n))
     return (summ%10)
 
-class init:
-    def __init__(self,value,key):
-        self.key=key
-        self.value=value
 
+
+
+class init:
+    def __init__(self,key):
+        self.key=key
+        self.value=[]
 class Hash:
     def __init__(self):
         self.array=[]
         for i in range(10):
-            self.array+=[init(None,None)]
+            self.array+=[init(None)]
     def add(self,value):
         key=Hashing(value)
-        if self.array[key].key!=None:
-            print 'Rewrite',
-            print self.array[key].value,
-            print '?[Yes/No]'
-            messege=raw_input ()
-            if messege == 'Yes':
-                self.array[key].key=key
-                self.array[key].value=value
-        else:  
-            self.array[key].key=key
-            self.array[key].value=value
-
+        self.array[key].value.append(value)
+        self.array[key].key=key
     def delite(self,value):
         key=Hashing(value)
-        if self.array[key].value!=None:    
-            self.array[key].key=None
-            self.array[key].value=None
-            print "True"
+        if self.array[key].value!=None:
+            i=0
+            while i<=(len(self.array[key].value)-1):
+                if self.array[key].value[i]==value:
+                    del self.array[key].value[i]
+                    print "True"
+                i+=1
         else:
             print "No Delite"
-
     def search(self,value):
         key=Hashing(value)
-        if self.array[key].value!=None:    
-            print ("Yes Key=",self.array[key].key)
+        if self.array[key].value!=None:
+            i=0
+            while i<=(len(self.array[key].value)-1):
+                if self.array[key].value[i]==value:
+                    print "Yes"
+                    print self.array[key].value
+                    print self.array[key].key
+                i+=1
+
         else:
             print "No"
-
     def print_hash(self):
         for i in range (len(self.array)):
-            print (self.array[i].value,self.array[i].key)       
+            print (self.array[i].value,self.array[i].key)
             print ('\n')
 
 
-n1=Hash()      
+n1=Hash() 
+
