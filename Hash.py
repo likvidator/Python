65fd6907ce1640acb634b3d8d857abdcb3115674
likvidator <max-kor4@yandex.ru>
2013-05-18 14:51:28 +0400

    Finish

--- Hash.py
+++ Hash.py
@@ -1,54 +1,50 @@
-def Hashing(value):
+def Hashing(value,zn):
     summ=0
     for n in str(value):
         summ+=int(ord(n))
-    return (summ%10)
-
-
-
-
+    if zn==True:
+        return (summ%10)
+    if zn==False:
+        return (summ)
 class init:
-    def __init__(self,key):
-        self.key=key
+    def __init__(self,key,value):
+        self.key=[]
         self.value=[]
+        self.hashkey=key
 class Hash:
     def __init__(self):
         self.array=[]
         for i in range(10):
-            self.array+=[init(None)]
-    def add(self,value):
-        key=Hashing(value)
-        self.array[key].value.append(value)
-        self.array[key].key=key
-    def delite(self,value):
-        key=Hashing(value)
-        if self.array[key].value!=None:
+            self.array+=[init(None,None)]
+    def add(self,key,value):
+        hashkey=Hashing(key,True)
+        self.array[hashkey].value.append(value)
+        self.array[hashkey].key.append(key)
+        self.array[hashkey].hashkey=hashkey
+    def delete(self,key):
+        hashkey=Hashing(key,True)
+        if self.array[hashkey].value!=None:
             i=0
-            while i<=(len(self.array[key].value)-1):
-                if self.array[key].value[i]==value:
-                    del self.array[key].value[i]
+            while i<=(len(self.array[hashkey].key)-1):
+                if self.array[hashkey].key[i]==key:
+                    del self.array[hashkey].value[i]
+                    del self.array[hashkey].key[i]
                     print "True"
                 i+=1
         else:
-            print "No Delite"
-    def search(self,value):
-        key=Hashing(value)
-        if self.array[key].value!=None:
+            print "No Delete"
+    def search(self,key):
+        hashkey=Hashing(key,True)
+        if self.array[hashkey].key!=None:
             i=0
-            while i<=(len(self.array[key].value)-1):
-                if self.array[key].value[i]==value:
-                    print "Yes"
-                    print self.array[key].value
-                    print self.array[key].key
+            while i<=(len(self.array[hashkey].key)-1):
+                if self.array[hashkey].key[i]==key:
+                    return (self.array[hashkey].value[i])
                 i+=1
-
-        else:
-            print "No"
     def print_hash(self):
         for i in range (len(self.array)):
             print (self.array[i].value,self.array[i].key)
+            print (self.array[i].hashkey)
             print ('\n')
-
-
 n1=Hash() 
 
