02aaaa389a210787753ff9dedc00c4d16ed70c7f
likvidator <max-kor4@yandex.ru>
2013-05-30 01:16:22 +0400

    Add floida

--- Graf.py
+++ Graf.py
@@ -1,22 +1,29 @@
 def printmatrix(N):
 	for i in range(len(N)):
 		print N[i]
-# def Deicstra(N,a,b):
-# 	d=N
-# 	s=[]
-# 	for i in range(len(N)):
-# 		s+=[0]
-# 	mark=[]
-# 	for i in range(len(N)):
-# 		mark+=[False]
-# 	s=d[a-1]
-# 	mark[a-1]=True
-# 	for n in range(len(N)):
-# 		for i in range(len(N)):
-# 			if (s[i]<d[n][i]) and not(mark[i]) :
-# 				s[i]=d[n][i]
-# 		markcd[n]=True
-# 	print s[b-1]
+# def deykstra(N,node):N
+# 		l = len(N)
+# 		if node > l  or node < 1:
+# 			print 'Error'
+# 		else:
+# 			node -= 1
+# 			visited = []
+# 			short_dist =[]
+# 			for i in range(l):
+# 				short_dist += [N[node][i]]
+# 				visited += [False]
+# 			visited[node] = True
+# 			for i in range(l):
+# 				m = 0
+# 				for j in range(l):
+# 					if(0 < short_dist[j] < m or m == 0) and not visited[j]:
+# 						m = short_dist[j]
+# 						k = j
+# 				visited[k] = True
+#                 for j in range(l):
+#                     if N[k][j] != 0 and (N[k][j] + short_dist[k] < short_dist[j] or short_dist[j] == 0) and not visited[j]:
+#                         short_dist[j] = N[k][j] + short_dist[k]
+#             print short_dist
 def Floida(N):
 	s=N
 	for k in range(len(s)):
@@ -58,15 +65,16 @@ def write_file(N):
 
 
 def write_for_graphviz(f,N):           
-	for i in range(len(N)-1):
-		for j in range(i+1,len(N)-1):
-			f.write (str(i))
-			f.write ("->")
-			f.write (str(j))
-			f.write ("[label=")
-			f.write (str(N[i][j]))
-			f.write ("]")
-			f.write ('\n')
+	for i in range(len(N)):
+		for j in range(i+1,len(N)):
+			if not(N[i][j]==0):
+				f.write (str(i))
+				f.write ("->")
+				f.write (str(j))
+				f.write ("[label=")
+				f.write (str(N[i][j]))
+				f.write ("]")
+				f.write ('\n')
 
 	
 		
